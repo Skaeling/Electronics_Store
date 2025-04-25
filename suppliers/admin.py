b6314d413b6e7e88d846eac99260cc57505c17e2
Lana Virina <lana.virina@gmail.com>
2025-04-25 03:45:08 +0300

    add admin action

--- suppliers/admin.py
+++ suppliers/admin.py
@@ -1,21 +1,23 @@
 from django.contrib import admin
-from .models import NetworkNode, Contact, Product
+from .models import NetworkNode, Contact
+
+
+def clear_arrears(modeladmin, request, queryset):
+    queryset.update(arrears=0.00)
+    modeladmin.message_user(request, 'Задолженность выбранных объектов сети погашена')
+
+
+clear_arrears.short_description = 'Обнулить задолженность перед поставщиками выбранных объектов сети'
 
 
 class ContactInline(admin.StackedInline):
     model = Contact
 
 
-# class ProductInline(admin.TabularInline):
-#     model = Product
-#     extra = 1
-
-
 @admin.register(NetworkNode)
 class NetworkNodeAdmin(admin.ModelAdmin):
     exclude = ['created_at']
-    list_display = ("__str__", 'supplier')
+    list_display = ("__str__", 'supplier', 'arrears')
     list_filter = ['contacts__city']
-    inlines = [ContactInline,
-               # ProductInline
-               ]
+    inlines = [ContactInline]
+    actions = [clear_arrears]
